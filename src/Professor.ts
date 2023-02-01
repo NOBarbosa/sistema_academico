e3597e6e27430ae4cc04a8f7a9121ac7c03902b3
Natalia Oliveira <nob.ufba@gmail.com>
2023-01-31 21:58:59 -0300

    feat:lancar nota

--- src/Professor.ts
+++ src/Professor.ts
@@ -1,11 +1,25 @@
+import { Aluno } from "./Aluno";
 import { TUsuario, Usuario } from "./Usuario";
 
-export class Professos extends Usuario{
+interface IProfessor {
+  lancarNota(aluno: Aluno, nota:number):void
+}
+
+
+export class Professos extends Usuario implements IProfessor{
   discplina: string
 
   constructor(dados:TUsuario, displina:string){
     super(dados)
     this.discplina = displina
+  }
+
+  lancarNota(aluno: Aluno, nota: number): void {
+
+    this.verificaLogin()
+    aluno.notas.push(nota);
+
+
   }
 
   private verificaLogin():void{
